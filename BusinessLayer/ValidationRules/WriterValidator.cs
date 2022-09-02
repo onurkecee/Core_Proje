8cbec64a6c533666cbe4a262724a26fd19537a78
Onur Keçe <onurkecee@gmail.com>
2022-09-02 13:23:41 +0300

    Login Paneli, FluentValidation

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Bu Alan Boş Geçilemez.");
+            RuleFor(x => x.WriterName).MinimumLength(3).WithMessage("Yazar Adı En Az 3 Karakterden Oluşturulmalıdır.");
+            RuleFor(x => x.WriterName).MaximumLength(30).WithMessage("Yazar Adı En Fazla 30 Karakterden Oluşturulmalıdır.");
+
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Bu Alan Boş Geçilemez");
+            RuleFor(x => x.WriterMail).Matches(@"[@,.]").WithMessage("Mail Adresi @ içermelidir.");
+
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Lütfen Şifreyi Giriniz");
+            RuleFor(x => x.WriterPassword).MinimumLength(3).WithMessage("Şifre En Az 3 Karakterden Oluşturulmalıdır.");
+            RuleFor(x => x.WriterPassword).MaximumLength(20).WithMessage("Şifre En Fazla 20 Karakterden Oluşturulmalıdır.");
+        }
+    }
+}
--- Core_Proje/Controllers/RegisterController.cs
+++ Core_Proje/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -22,10 +24,24 @@ namespace Core_Proje.Controllers
         [HttpPost]
         public IActionResult Index(Writer p)
         {
-            p.WriterStatus = true;
-            p.WriterAbout = "Deneme Yazısı";
-            wm.WriterAdd(p);
-            return RedirectToAction("Index", "Blog");
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(p);
+            if (results.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Deneme Yazısı";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
         }
     }
 }
